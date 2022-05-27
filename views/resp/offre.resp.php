5385ec407c28b854b8b05035d9701a8799086880
w0l1d <49680822+w0l1d@users.noreply.github.com>
2022-05-28 00:12:14 +0100

    gestion des offres in new dashboard with datatable

--- views/resp/offre.resp.php
+++ views/resp/offre.resp.php
@@ -8,173 +8,160 @@ $curr_user = $_SESSION['user'];
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
-    <link rel="stylesheet" type="text/css"
-          href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/datatables.min.css"/>
-
-    <script rel="stylesheet" src="assets/js/bootstrap.min.js"></script>
-
-    <script type="text/javascript"
-            src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/datatables.min.js"></script>
-
-    <link rel="stylesheet" href="/assets/css/Profil.css">
-    <!--   <link rel="stylesheet" href="/css/bootstrap.min.css"> -->
-    <!-- icons-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Gestion des Offres</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/datatable/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="assets/datatable/css/responsive.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
 </head>
-<body>
-<div class="container">
-    <vmenu>
-        <div class="top">
-            <div class="logo">
-                <img src="/assets/pic/img.png">
-                <h2>Stage<span class="primary">FSTM</span></h2>
-            </div>
-            <div class="close" id="close-btn">
-                <span class="material-icons-sharp">close</span>
-            </div>
-        </div>
-        <div class="sidebar">
-            <a href="#">
-                <span class="material-icons-sharp">dashboard</span>
-                <h3>Dashboard</h3>
-            </a>
-            <a href="#">
-                <span class="material-icons-sharp"> person</span>
-                <h3>Etudiants</h3>
-            </a>
-            <a href="#">
-                <span class="material-icons-sharp"> work_history</span>
-                <h3>Stages</h3>
-            </a>
 
-            <a href="#">
-                <span class="material-icons-sharp">person_add</span>
-                <h3>validation</h3>
-                <span class="validation-count">6</span>
-            </a>
-            <a href="#">
-                <span class="material-icons-sharp"> business</span>
-                <h3>Societes</h3>
-            </a>
-            <a href="#">
-                <span class="material-icons-sharp"> post_add</span>
-                <h3>Offres de Stage</h3>
-            </a>
-            <a href="#" class="active">
-                <span class="material-icons-sharp"> edit</span>
-                <h3>Modifier profil</h3>
-            </a>
-
-
-            <a href="#">
-                <span class="material-icons-sharp">logout</span>
-                <h3>Deconnection</h3>
-            </a>
-
-
-        </div>
-    </vmenu>
-
-
-    <!-- end of vmenu-->
-    <main>
-        <div class="topSide">
-            <h1>Modifier Profil</h1>
-            <div class="top">
-                <button id="menu-btn">
-                    <span class="material-icons-sharp">menu</span>
-                </button>
-                <div class="theme-toggler">
-                    <span class="material-icons-sharp active">light_mode</span>
-                    <span class="material-icons-sharp ">dark_mode</span>
+<body id="page-top">
+<div id="wrapper">
+    <?php
+    require_once 'parts/sidebar.html'
+    ?>
+    <div class="d-flex flex-column" id="content-wrapper">
+        <div id="content">
+            <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+                <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+                    <form class="d-none d-sm-inline-block me-auto ms-md-3 my-2 my-md-0 mw-100 navbar-search">
+                        <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ..."><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
+                    </form>
+                    <ul class="navbar-nav flex-nowrap ms-auto">
+                        <li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><i class="fas fa-search"></i></a>
+                            <div class="dropdown-menu dropdown-menu-end p-3 animated--grow-in" aria-labelledby="searchDropdown">
+                                <form class="me-auto navbar-search w-100">
+                                    <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ...">
+                                        <div class="input-group-append"><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
+                                    </div>
+                                </form>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown no-arrow">
+                            <div class="nav-item dropdown no-arrow">
+                                <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">
+                                        <span class="d-none d-lg-inline me-2 text-gray-600 small">
+                                            <?php echo "{$curr_user['lname']} {$curr_user['fname']}" ?><br>
+                                            <small class="text-muted"><?php echo $curr_user['type']; ?></small>
+                                        </span>
+                                    <img class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg">
+                                </a>
+                                <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in">
+                                    <a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Profile</a>
+                                    <a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Settings</a>
+                                    <a class="dropdown-item" href="#"><i class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Activity log</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="/login?logout"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout</a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-                <div class="profile">
-                    <div class="info">
-                        <p> Hey, <?php echo "{$curr_user['lname']} {$curr_user['fname']}" ?></p>
-                        <small class="text-muted"><?php echo $curr_user['type']; ?></small>
+            </nav>
+            <div class="container-fluid">
+                <div class="d-sm-flex justify-content-between align-items-center mb-4">
+                    <h3 class="text-dark mb-0">Offres de stages<br></h3><a class="btn btn-primary d-none d-sm-inline-block" role="button" href="#"><i class="fas fa-plus fa-sm text-white-50"></i>&nbsp;ajouter offre</a>
+                </div>
+                <div class="card shadow">
+                    <div class="card-header py-3">
+                        <p class="text-primary m-0 fw-bold">Offres</p>
                     </div>
-                    <div class="profile-photo">
-                        <img src="/assets/pic/profile.jpg">
+                    <div class="card-body">
+                        <table id="myTable" class="table table-striped nowrap" style="width:100%">
+                            <thead>
+                            <th>id</th>
+                            <th>title</th>
+                            <th>type_stage</th>
+                            <th>nbr_stagiaire</th>
+                            <th>description</th>
+                            <th>duree_stage</th>
+                            <th>statue</th>
+                            </thead>
+                            <?php
+                            try {
+                                $query = "SELECT id, created_date, delai_offre, description, duree_stage, end_stage,
+                                nbr_stagiaire, start_stage, statue, title, updated_date, entreprise_id,
+                                formation_id, type_stage FROM offre e
+                                where formation_id = (SELECT id FROM formation
+                                                                WHERE responsable_id = :id_resp limit 1)";
+
+                                $stmt = $pdo->prepare($query);
+                                $stmt->bindParam('id_resp', $curr_user['id']);
+                                $stmt->execute();
+                                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                            if (!empty($rows)) {
+                            foreach ($rows as $key => $value) {
+                                ?>
+                                <tr>
+                                    <td><?php echo $value['id']; ?></td>
+                                    <td><?php echo $value['title']; ?></td>
+                                    <td><?php echo $value['type_stage']; ?></td>
+                                    <td><?php echo $value['nbr_stagiaire']; ?></td>
+                                    <td><?php echo $value['description']; ?></td>
+                                    <td><?php echo $value['duree_stage']; ?></td>
+                                    <td><?php echo $value['statue']; ?></td>
+                                </tr>
+                            <?php
+                            }
+                            }
+                            } catch (Exception $e) {
+                                echo 'Erreur : ' . $e->getMessage();
+                            }
+                            ?>
+
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="container1">
-            <table id="myTable">
-                <thead>
-                <th>id</th>
-                <th>title</th>
-                <th>nbr etuds</th>
-                <th>description</th>
-                <th>duree stage</th>
-                <th>status</th>
-                </thead>
-                <?php
-                try {
-                    $query = "SELECT id, created_date, delai_offre, description, duree_stage, end_stage,
-       nbr_stagiaire, start_stage, statue, title, updated_date, entreprise_id,
-       formation_id, type_stage FROM offre e ";
-
-                    $stmt = $pdo->prepare($query);
-                    $stmt->execute();
-                    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                if (!empty($rows)) {
-                foreach ($rows as $key => $value) {
-                    ?>
-                    <tr>
-                        <td><?php echo $value['id']; ?></td>
-                        <td><?php echo $value['title']; ?></td>
-                        <td><?php echo $value['type_stage']; ?></td>
-                        <td><?php echo $value['nbr_stagiaire']; ?></td>
-                        <td><?php echo $value['description']; ?></td>
-                        <td><?php echo $value['duree_stage']; ?></td>
-                        <td><?php echo $value['statue']; ?></td>
-                    </tr>
-                <?php
-                }
-                }
-                } catch (Exception $e) {
-                    echo 'Erreur : ' . $e->getMessage();
-                } finally {
-                ?>
-
-                    <script>
-                        $(document).ready(function () {
-                            $('#myTable').DataTable({
-                                responsive: true,
-                                "language": {
-                                    "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/fr-FR.json"
-                                }
-                            });
-                        });
-                    </script>
-                    <?php
-                }
-                ?>
-
-            </table>
-        </div>
-
-    </main>
-    <!--------------------End of main------------->
-
-
+        <footer class="bg-white sticky-footer">
+            <div class="container my-auto">
+                <div class="text-center my-auto copyright"><span>Copyright © Gestion de stage 2022</span></div>
+            </div>
+        </footer>
+    </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
 </div>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/datatable/js/dataTables.bootstrap5.min.js"></script>
+<script src="assets/datatable/js/jquery.dataTables.min.js"></script>
+<script src="assets/datatable/js/dataTables.responsive.min.js"></script>
+<script src="assets/datatable/js/responsive.bootstrap5.min.js"></script>
+<script src="assets/js/bs-init.js"></script>
+<script src="assets/js/theme.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#myTable').DataTable({
+            responsive: {
+                details: {
+                    display: $.fn.dataTable.Responsive.display.modal({
+                        header: function (row) {
+                            var data = row.data();
+                            return 'Details for ' + data[0] + ' ' + data[1];
+                        }
+                    }),
+                    renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+                        tableClass: 'table'
+                    })
+                }
+            },
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/fr-FR.json"
+            }
+        });
+    });
+</script>
 
-<script src="/assets/js/code.js"></script>
-<!--<script src="jquery.js"></script>-->
-
-
-<!-- <script src="/js/bootstrap.bundle.min.js"></script>-->
 </body>
+
 </html>
 
 
