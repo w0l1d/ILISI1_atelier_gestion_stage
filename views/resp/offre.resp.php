bcfadade93a008159f7047aec24ae1cb1d125c70
w0l1d <49680822+w0l1d@users.noreply.github.com>
2022-05-27 10:08:57 +0100

    fixed images link in dashboard.resp.php

--- views/resp/offre.resp.php
+++ views/resp/offre.resp.php
@@ -13,31 +13,81 @@ $curr_user = $_SESSION['user'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- icons-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <script rel="stylesheet" src="assets/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" type="text/css"
           href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/datatables.min.css"/>
 
+    <script rel="stylesheet" src="assets/js/bootstrap.min.js"></script>
+
     <script type="text/javascript"
             src="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.12.1/datatables.min.js"></script>
 
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/Profil.css">
+    <!--   <link rel="stylesheet" href="/css/bootstrap.min.css"> -->
+    <!-- icons-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
 
-    <title>intership </title>
 </head>
 <body>
 <div class="container">
+    <vmenu>
+        <div class="top">
+            <div class="logo">
+                <img src="/assets/pic/img.png">
+                <h2>Stage<span class="primary">FSTM</span></h2>
+            </div>
+            <div class="close" id="close-btn">
+                <span class="material-icons-sharp">close</span>
+            </div>
+        </div>
+        <div class="sidebar">
+            <a href="#">
+                <span class="material-icons-sharp">dashboard</span>
+                <h3>Dashboard</h3>
+            </a>
+            <a href="#">
+                <span class="material-icons-sharp"> person</span>
+                <h3>Etudiants</h3>
+            </a>
+            <a href="#">
+                <span class="material-icons-sharp"> work_history</span>
+                <h3>Stages</h3>
+            </a>
 
-    <!-- start of vmenu-->
-    <?php require_once __DIR__ . '/parts/sidebar.html' ?>
+            <a href="#">
+                <span class="material-icons-sharp">person_add</span>
+                <h3>validation</h3>
+                <span class="validation-count">6</span>
+            </a>
+            <a href="#">
+                <span class="material-icons-sharp"> business</span>
+                <h3>Societes</h3>
+            </a>
+            <a href="#">
+                <span class="material-icons-sharp"> post_add</span>
+                <h3>Offres de Stage</h3>
+            </a>
+            <a href="#" class="active">
+                <span class="material-icons-sharp"> edit</span>
+                <h3>Modifier profil</h3>
+            </a>
+
+
+            <a href="#">
+                <span class="material-icons-sharp">logout</span>
+                <h3>Deconnection</h3>
+            </a>
+
+
+        </div>
+    </vmenu>
+
+
     <!-- end of vmenu-->
-
     <main>
         <div class="topSide">
-            <h1>Offres de stage</h1>
+            <h1>Modifier Profil</h1>
             <div class="top">
                 <button id="menu-btn">
                     <span class="material-icons-sharp">menu</span>
@@ -52,7 +102,7 @@ $curr_user = $_SESSION['user'];
                         <small class="text-muted"><?php echo $curr_user['type']; ?></small>
                     </div>
                     <div class="profile-photo">
-                        <img src="/assets/img/profile.jpg">
+                        <img src="/assets/pic/profile.jpg">
                     </div>
                 </div>
             </div>
@@ -69,7 +119,7 @@ $curr_user = $_SESSION['user'];
                 </thead>
                 <?php
                 try {
-                    $query = "SELECT id, created_date, delai_offre, description, duree_stage, end_stage, 
+                    $query = "SELECT id, created_date, delai_offre, description, duree_stage, end_stage,
        nbr_stagiaire, start_stage, statue, title, updated_date, entreprise_id,
        formation_id, type_stage FROM offre e ";
 
@@ -112,12 +162,18 @@ $curr_user = $_SESSION['user'];
 
             </table>
         </div>
+
     </main>
     <!--------------------End of main------------->
 
-</div>
-<script src="/assets/js/code.js"></script>
 
+</div>
+
+<script src="/assets/js/code.js"></script>
+<!--<script src="jquery.js"></script>-->
+
+
+<!-- <script src="/js/bootstrap.bundle.min.js"></script>-->
 </body>
 </html>
 
