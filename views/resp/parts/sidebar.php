f653722f616c0bf0e4ad03d409ac6ccd3f5ab38b
w0l1d <49680822+w0l1d@users.noreply.github.com>
2022-05-30 20:21:24 +0100

    respansable :   -> offers   -> etudiant   -> entreprise  todo :  -> main dashboard

--- views/resp/parts/sidebar.php
+++ views/resp/parts/sidebar.php
@@ -14,7 +14,7 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/etudiants">
-                    <i class="fa fa-gift"></i>
+                    <i class="fa fa-users"></i>
                     <span>Etudiants</span>
                 </a>
             </li>
