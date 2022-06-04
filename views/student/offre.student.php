1f412d076213615df179f5284ea0e57430a59768
KhaoulaBel <95535082+bel-k@users.noreply.github.com>
2022-06-04 07:23:10 +0100

    Update offre.student.php

--- views/student/offre.student.php
+++ views/student/offre.student.php
@@ -181,9 +181,11 @@ if(isset($_POST['button1']) ) {
 <script src="/assets/datatable/js/dataTables.responsive.min.js"></script>
 <script src="/assets/datatable/js/responsive.bootstrap5.min.js"></script>
 
+
 <script>
     $(document).ready(function () {
         $('#myTable').DataTable({
+            
             responsive: {
                 details: {
                     display: $.fn.dataTable.Responsive.display.modal({
@@ -200,6 +202,8 @@ if(isset($_POST['button1']) ) {
             "language": {
                 "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/fr-FR.json"
             }
+
+        
         });
     });
 </script>
