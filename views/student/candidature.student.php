a28c90bec3c5841b2cf396b2469bd35e0e975f04
w0l1d <49680822+w0l1d@users.noreply.github.com>
2022-06-14 14:49:40 +0100

    -> set candidature status

--- views/student/candidature.student.php
+++ views/student/candidature.student.php
@@ -50,12 +50,12 @@ if (!empty($_GET['agree'])) {
         }
 
         $query = "UPDATE candidature SET status = 'ACCEPTED' 
-                   WHERE id in (SELECT id FROM candidature c 
+                   WHERE id = (SELECT id FROM candidature c 
                                          WHERE c.status LIKE 'WAITING' 
                                            AND c.offre_id = (SELECT offre_id 
                                                              FROM candidature 
                                                              WHERE id = :candidature_id)  
-                                           AND (c.position, c.offre_id) = 
+                                           AND (c.position, c.offre_id) in 
                                                (SELECT min(c2.position), c2.offre_id 
                                                 FROM candidature c2 GROUP BY c2.offre_id))";
 
